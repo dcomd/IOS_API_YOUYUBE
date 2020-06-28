930ee396c984c82823884610cd14c8a99a23cf8a
Alexandre <alexandre.nepomuceno@c6bank.com>
2020-06-28 15:11:48 -0300

    Initial Commit

---
+++ YoutubePorject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubePorject
+//
+//  Created by Alexandre de Oliveira Nepomuceno on 28/06/20.
+//  Copyright © 2020 Alexandre de Oliveira Nepomuceno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
